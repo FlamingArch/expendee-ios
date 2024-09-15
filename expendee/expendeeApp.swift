b2680bdc7cdb3fc8b91ff9681357b627a30653c1
Harsh Chaturvedi <16736449+FlamingArch@users.noreply.github.com>
2024-09-15 19:11:12 +0530

    Initial Commit

---
+++ expendee/expendeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  expendeeApp.swift
+//  expendee
+//
+//  Created by Harsh Chaturvedi on 15/09/24.
+//
+
+import SwiftUI
+
+@main
+struct expendeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
